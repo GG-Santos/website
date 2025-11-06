ac9e69c02925fab992531c3302ea7f0defa61c9e
GG-Santos <ggsantos_0415@proton.me>
2025-11-06 21:21:19 +0800

    Fix import paths to use generated/client/client instead of generated/client

--- scripts/test-db-connection.ts
+++ scripts/test-db-connection.ts
@@ -6,7 +6,7 @@
  */
 
 import "dotenv/config";
-import { PrismaClient } from "../generated/client";
+import { PrismaClient } from "../generated/client/client";
 
 const prisma = new PrismaClient();
 
